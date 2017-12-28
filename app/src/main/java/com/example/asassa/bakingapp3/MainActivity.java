cc48524c8b7ff11755576ce5563d0583cd8594ce
ASassa <vrienden717>
2017-12-28 17:31:08 +0600

    Added network load

--- app/src/main/java/com/example/asassa/bakingapp3/MainActivity.java
+++ app/src/main/java/com/example/asassa/bakingapp3/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.asassa.bakingapp3;
 
+import android.content.AsyncTaskLoader;
+import android.content.Context;
 import android.os.AsyncTask;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AdapterView;
 
 import com.example.asassa.bakingapp3.Utils.NetworkProvider;
 import com.example.asassa.bakingapp3.Utils.Recipe;
@@ -11,15 +16,40 @@ import java.net.URI;
 import java.net.URL;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks {
 
     List<Recipe> mRecipes = null;
+    Context mContext = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = this;
         new RecipeRequest().execute();
+
+    }
+
+
+    @Override
+    public Loader onCreateLoader(int id, Bundle args) {
+
+        return new android.support.v4.content.AsyncTaskLoader<List<Recipe>>(mContext) {
+            @Override
+            public List<Recipe> loadInBackground() {
+                List<Recipe> recipes = NetworkProvider.getRecipesJSON("https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json");
+                return recipes;
+            }
+        };
+    }
+
+    @Override
+    public void onLoadFinished(Loader loader, Object data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader loader) {
 
     }
 
